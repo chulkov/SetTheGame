b6d91f51146e3905cbde71cc474fce05169f0372
olegpon <oleg.ponomarev@rasolution.ru>
2019-10-28 10:51:15 +0500

    Initial Commit

---
+++ SetTheGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetTheGame
+//
+//  Created by Max on 28/10/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
