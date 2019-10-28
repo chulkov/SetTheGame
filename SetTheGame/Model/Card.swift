9b204d7fb167e7f6573d62bdf6346199a1caaae6
olegpon <oleg.ponomarev@rasolution.ru>
2019-10-28 17:25:35 +0500

    first commit

---
+++ SetTheGame/Model/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  SetTheGame
+//
+//  Created by Max on 28/10/2019.
+//
+
+import Foundation
---
+++ SetTheGame/Model/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  SetTheGame
+//
+//  Created by Max on 28/10/2019.
+//
+
+import Foundation
